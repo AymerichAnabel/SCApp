6bf2997e6411ebd34c18f750f5fd26ed13a04cb6
anabel <aymerichanabel@gmail.com>
2022-10-01 14:44:08 -0400

    CalendarioListo

--- app/src/main/java/com/aymerichanabel/scapp/MainActivity.java
+++ app/src/main/java/com/aymerichanabel/scapp/MainActivity.java
@@ -46,14 +46,12 @@ public class MainActivity extends AppCompatActivity implements CalendarAdapter.O
     private void setMonthView()
     {
         monthYearText.setText(monthYearFromDate(CalendarUtils.selectedDate));
-        ArrayList<LocalDate> daysInMonth = daysInMonthArray(CalendarUtils.selectedDate);
+        ArrayList<LocalDate> daysInMonth = daysInMonthArray();
 
         CalendarAdapter calendarAdapter = new CalendarAdapter(daysInMonth, this);
         RecyclerView.LayoutManager layoutManager = new GridLayoutManager(getApplicationContext(), 7);
         calendarRecyclerView.setLayoutManager(layoutManager);
         calendarRecyclerView.setAdapter(calendarAdapter);
-
-
     }
 
 
