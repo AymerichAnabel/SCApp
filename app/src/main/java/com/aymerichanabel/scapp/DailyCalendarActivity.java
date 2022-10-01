bf9bc87fe5279ad113d482058f09c5b60f2f2979
anabel <aymerichanabel@gmail.com>
2022-10-01 13:12:06 -0400

    Initial commit

---
+++ app/src/main/java/com/aymerichanabel/scapp/DailyCalendarActivity.java
@@ -0,0 +1,68 @@
+package com.aymerichanabel.scapp;
+
+import static com.aymerichanabel.scapp.CalendarUtils.selectedDate;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.time.format.TextStyle;
+import java.util.Locale;
+
+public class DailyCalendarActivity extends AppCompatActivity
+{
+
+    private TextView monthDayText;
+    private TextView dayOfWeekTV;
+    private ListView hourListView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_daily_calendar);
+        initWidgets();
+    }
+
+    private void initWidgets()
+    {
+        monthDayText = findViewById(R.id.monthDayText);
+        dayOfWeekTV = findViewById(R.id.dayOfWeekTV);
+        hourListView = findViewById(R.id.hourListView);
+    }
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        setDayView();
+    }
+
+    private void setDayView()
+    {
+        monthDayText.setText(CalendarUtils.monthDayFromDate(selectedDate));
+        String dayOfWeek = selectedDate.getDayOfWeek().getDisplayName(TextStyle.FULL, Locale.getDefault());
+        dayOfWeekTV.setText(dayOfWeek);
+    }
+
+    public void peviousDayAction(View view)
+    {
+        CalendarUtils.selectedDate = CalendarUtils.selectedDate.minusDays(1);
+        setDayView();
+    }
+
+    public void nextDayAction(View view)
+    {
+        CalendarUtils.selectedDate = CalendarUtils.selectedDate.plusDays(1);
+        setDayView();
+    }
+
+    public void newEventAction(View view)
+    {
+        startActivity(new Intent(this, EventEditActivity.class));
+    }
+}
---
+++ app/src/main/java/com/aymerichanabel/scapp/WeekViewActivity.java
@@ -0,0 +1,102 @@
+package com.aymerichanabel.scapp;
+
+import static com.aymerichanabel.scapp.CalendarUtils.daysInMonthArray;
+import static com.aymerichanabel.scapp.CalendarUtils.daysInWeekArray;
+import static com.aymerichanabel.scapp.CalendarUtils.monthYearFromDate;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class WeekViewActivity extends AppCompatActivity implements CalendarAdapter.OnItemListener
+{
+
+    private TextView monthYearText;
+    private RecyclerView calendarRecyclerView;
+    private ListView eventListView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_week_view);
+        initWidgets();
+        setWeekView();
+    }
+
+    private void initWidgets()
+    {
+        calendarRecyclerView = findViewById(R.id.calendarRecyclerView);
+        monthYearText = findViewById(R.id.monthYearTV);
+        eventListView = findViewById(R.id.eventListView);
+    }
+
+    private void setWeekView()
+    {
+        monthYearText.setText(monthYearFromDate(CalendarUtils.selectedDate));
+        ArrayList<LocalDate> days = daysInWeekArray(CalendarUtils.selectedDate);
+
+        CalendarAdapter calendarAdapter = new CalendarAdapter(days, this);
+        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(getApplicationContext(), 7);
+        calendarRecyclerView.setLayoutManager(layoutManager);
+        calendarRecyclerView.setAdapter(calendarAdapter);
+        setEventAdapter();
+
+
+    }
+
+
+    public void peviousWeekAction(View view)
+    {
+        CalendarUtils.selectedDate = CalendarUtils.selectedDate.minusWeeks(1);
+        setWeekView();
+    }
+
+    public void nextWeekAction(View view)
+    {
+        CalendarUtils.selectedDate = CalendarUtils.selectedDate.plusWeeks(1);
+        setWeekView();
+    }
+
+    @Override
+    public void onItemClick(int position, LocalDate date)
+    {
+        CalendarUtils.selectedDate = date;
+        setWeekView();
+
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setEventAdapter();
+    }
+
+    private void setEventAdapter()
+    {
+        ArrayList<Event> dailyEvents = Event.eventsForDate(CalendarUtils.selectedDate);
+        EventAdapter eventAdapter = new EventAdapter(getApplicationContext(), dailyEvents);
+        eventListView.setAdapter(eventAdapter);
+
+    }
+
+    public void newEventAction(View view)
+    {
+        startActivity(new Intent(this, EventEditActivity.class));
+    }
+
+    public void dailyAction(View view)
+    {
+        startActivity(new Intent(this, DailyCalendarActivity.class));
+    }
+}
